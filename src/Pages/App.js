af67497fdffe791b84914692d509b4dcc2421a33
duck202 <guyeldan@yahoo.com>
2020-01-16 12:48:39 +0200

    Start-CreatepagesFiles

---
+++ src/Pages/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import autoBind from 'react-autobind';
+import shortid from 'shortid'
+import Home from './Home'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+    autoBind(this);
+  }
+  render() {
+    return (
+      <div>
+        <Home/>
+      </div>
+    )
+  }
+}
+
+
