f2e993bdc4caf38e3c2c1e2e368b20390ee95c2b
duck202 <guyeldan@yahoo.com>
2020-01-21 09:33:02 +0200

    lading_page_start

--- src/Pages/LandPage.js
+++ src/Pages/LandPage.js
@@ -15,30 +15,65 @@ export default class LanPage extends Component {
   }
   render() {
     return (
-      <div>
-        <Container>
-          <Row>
-            <Col>
-                <Figure>
-  <Figure.Image 
-    width={500}
-    height={180}
-    alt="171x180"
-    src="https://images.pexels.com/photos/830891/pexels-photo-830891.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-  />
-    <Figure.Caption>
-  מערכת ניהול בתים המשותפים מלאה - מלמטה עד למעלה.
-  </Figure.Caption>
-  <Figure.Caption>
-    ברוכים הבאים למערכת ניהול ועדי בית. כאן תוכלו למצוא את כל אשר תצטרכו לצורך ניהול הבית המשותף בצורה קלה פשוטה וידידותית. 
-  </Figure.Caption>
-</Figure>  
+      <div className="borderg bcon"> 
+          <Row className="borderg">
+            <Col className="borderg">
+              <div className="scon">
+                <Row>
+                  <Col>
+                  <div  className="borderg uline">
+                  </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col><div className="borderg bline">3</div></Col>
+                  <Col><div className="borderg bline"> 2</div></Col>
+                  <Col><div className="borderg bline">1</div></Col>
+                </Row>
+              </div>
             </Col>
           </Row>
-        </Container>
       </div>
     )
   }
 }
 
 
+
+
+
+
+
+
+
+
+
+// <div className="topbox">
+// <img src="https://images.pexels.com/photos/830891/pexels-photo-830891.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" width="100%"/> 
+//                 <div>
+//    מלמטה עד למעלה - מערכת ניהול בתים המשותפים מלאה.
+//    </div>
+//    <br></br>
+//    <div>
+//    ברוכים הבאים למערכת ניהול ועדי בית. כאן תוכלו למצוא את כל אשר תצטרכו לצורך ניהול הבית המשותף בצורה קלה פשוטה וידידותית. 
+//    </div>
+// </div>
+
+
+
+//  <Figure className="figure-top border" >
+// <Figure.Image 
+//   width={500}
+//   height={180}
+//   alt="171x180"
+//   src="
+// >
+  
+// </Figure.Image>
+//   <Figure.Caption>
+// מלמטה עד למעלה - מערכת ניהול בתים המשותפים מלאה.
+// </Figure.Caption>
+// <Figure.Caption width={500} >
+//   ברוכים הבאים למערכת ניהול ועדי בית. כאן תוכלו למצוא את כל אשר תצטרכו לצורך ניהול הבית המשותף בצורה קלה פשוטה וידידותית. 
+// </Figure.Caption>
+// </Figure>   
